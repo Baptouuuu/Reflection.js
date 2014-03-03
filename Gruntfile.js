58be5001cc055c2a79e378884575f965aba32ff1
Baptiste Langlade <baptouuuu@gmail.com>
2014-03-03 21:04:04 +0100

    base repo files added

---
+++ Gruntfile.js
@@ -0,0 +1,179 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %>#<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            build: {
+                files: {
+                    'reflection.min.js': [
+                        'src/ReflectionClass.js',
+                        'src/ReflectionMethod.js',
+                        'src/ReflectionObject.js',
+                        'src/ReflectionProperty.js',
+                    ]
+                }
+            }
+        },
+        shell: {
+            test: {
+                command: 'venus run -t tests/ -n --singleton',
+                options: {
+                    stdout: true
+                }
+            }
+        },
+        watch: {
+            js: {
+                files: ['src/*.js'],
+                tasks: [],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        jscs: {
+            src: 'src/**/*.js',
+            options: {
+                requireCurlyBraces: [
+                    'if',
+                    'else',
+                    'for',
+                    'do',
+                    'while',
+                    'try',
+                    'catch',
+                ],
+                requireSpaceAfterKeywords: [
+                    'if',
+                    'else',
+                    'for',
+                    'do',
+                    'while',
+                    'switch',
+                    'try',
+                    'catch'
+                ],
+                requireParenthesesAroundIIFE: true,
+                requireSpacesInFunctionExpression: {
+                    beforeOpeningRoundBrace: true,
+                    beforeOpeningCurlyBrace: true
+                },
+                disallowSpacesInsideObjectBrackets: true,
+                disallowSpacesInsideArrayBrackets: true,
+                disallowSpacesInsideParentheses: true,
+                disallowQuotedKeysInObjects: true,
+                disallowSpaceAfterObjectKeys: true,
+                requireCommaBeforeLineBreak: true,
+                requireOperatorBeforeLineBreak: [
+                    '?',
+                    '+',
+                    '-',
+                    '/',
+                    '*',
+                    '=',
+                    '==',
+                    '===',
+                    '!=',
+                    '!==',
+                    '>',
+                    '>=',
+                    '<',
+                    '<='
+                ],
+                disallowLeftStickedOperators: [
+                    '?',
+                    '+',
+                    '-',
+                    '/',
+                    '*',
+                    '=',
+                    '==',
+                    '===',
+                    '!=',
+                    '!==',
+                    '>',
+                    '>=',
+                    '<',
+                    '<='
+                ],
+                requireRightStickedOperators: ['!'],
+                disallowRightStickedOperators: [
+                    '?',
+                    '+',
+                    '/',
+                    '*',
+                    ':',
+                    '=',
+                    '==',
+                    '===',
+                    '!=',
+                    '!==',
+                    '>',
+                    '>=',
+                    '<',
+                    '<='
+                ],
+                requireLeftStickedOperators: [','],
+                disallowSpaceAfterPrefixUnaryOperators: [
+                    '++',
+                    '--',
+                    '+',
+                    '-',
+                    '~',
+                    '!'
+                ],
+                disallowSpaceBeforePostfixUnaryOperators: [
+                    '++',
+                    '--'
+                ],
+                requireSpaceBeforeBinaryOperators: [
+                    '+',
+                    '-',
+                    '/',
+                    '*',
+                    '=',
+                    '==',
+                    '===',
+                    '!=',
+                    '!=='
+                ],
+                requireSpaceAfterBinaryOperators: [
+                    '+',
+                    '-',
+                    '/',
+                    '*',
+                    '=',
+                    '==',
+                    '===',
+                    '!=',
+                    '!=='
+                ],
+                requireCamelCaseOrUpperCaseIdentifiers: true,
+                disallowKeywords: ["with"],
+                disallowMultipleLineBreaks: true,
+                validateQuoteMarks: '\'',
+                disallowMixedSpacesAndTabs: true,
+                disallowTrailingWhitespace: true,
+                disallowKeywordsOnNewLine: ['else'],
+                requireDotNotation: true,
+                validateJSDoc: {
+                    checkParamNames: true,
+                    checkRedundantParams: true,
+                    requireParamTypes: true
+                }
+            }
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', ['bower-install']);
+    grunt.registerTask('test', [
+        'jscs',
+        'shell:test'
+    ]);
+
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Reflection.js
+
+This library is here to help you manipulate and get informations about objects.
